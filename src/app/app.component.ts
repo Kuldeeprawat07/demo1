cafbad1d96f38426c9182ea0d16e65112f437429
Ahl.Nilsen <ahl.nilsen@uib.no>
2020-02-01 09:52:17 +0100

    Add config and setup for sentry integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-ng-app';
+
+  raiseError() {
+    throw new Error('Oops something went wrong');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 import { AppComponent } from './app.component';
 
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'https://e94479259c7e435fadfa0eb3c0ad69ee@sentry.io/2146128'
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    const eventId = Sentry.captureException(error.originalError || error);
+    Sentry.showReportDialog({ eventId });
+  }
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
